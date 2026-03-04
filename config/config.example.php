82da713de8620a79ccdeca5e839f088e31785478
Kéo <keo.ployer@ynov.com>
2026-03-04 23:39:45 +0100

    config.example

---
+++ config/config.example.php
@@ -0,0 +1,7 @@
+<?php
+
+// Fichier de configuration pour l'API TMDB, remplace les valeurs par vos propres clés et URL
+
+define('API_KEY', 'VOTRE_CLE_API_TMDB_ICI');
+define('API_URL', 'https://api.themoviedb.org/3');
+define('LANGUAGE', 'fr-FR');
